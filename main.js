ee9ccb2ee488f58c1fd536860e081c7ead24721a
amy20060411-spec <amy20060411@gmail.com>
2025-12-29 10:27:32 +0800

    1

---
+++ main.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'aos/dist/aos.css'
+import AOS from 'aos'
+import './styles/main.scss'
+
+AOS.init({
+  duration: 1000,
+  once: true
+})
